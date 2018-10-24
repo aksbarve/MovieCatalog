6f39715a35f10379a67200077a990f51ed67f3df
aksbarve <aksbarve@gmail.com>
2018-10-24 18:00:24 -0500

    movie details component

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../movie/movie';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { MovieService } from '../movie.service';
+
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
-  @Input() movie: Movie;
+  movie: Movie;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getMovie();
   }
 
+  goBack(): void {
+    this.location.back();
 }
+
+getMovie(): void {
+  const id = +this.route.snapshot.paramMap.get('id');
+  this.movieService.getMovie(id)
+    .subscribe(movie => this.movie = movie);
+}
+}
+
